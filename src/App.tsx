984201e79bb36432867fcfb8c18ca1ab7b47dad7
alanb33 <ab33@protonmail.com>
2025-07-06 09:36:29 -0400

    Game state manangement

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import Chessboard from "./components/Chessboard"
+import GameStateManager from "./utils/GameStateManager";
 import "./App.css"
 
 function App() {
-
-  return (
-    <>
+    const [, doChessboardUpdate] = useState<number>(0);
+    
+    const gameStateManager = GameStateManager.getInstance()
+    const resetButton =
+    <button
+    onClick={() => {
+        gameStateManager.resetGame();
+        // Force render update
+        doChessboardUpdate(prev => prev + 1);
+    }}
+    style={{
+        position: "absolute",
+        top: "10px",
+        left: "10px",
+        zIndex: 1000,
+        padding: "10px 20px",
+        backgroundColor: "#ff4444",
+        color: "white",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer"
+    }}
+    >
+    Reset Game
+    </button>
+    
+    return (
+        <>
+        {resetButton}
         <Chessboard />
-    </>
-  );
+        </>
+    );
 }
 
 export default App
